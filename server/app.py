3c5af54bc6b32464ccddfbb7a5c9f95c5e327984
capitalflowai <capitalflowai@gmail.com>
2023-08-19 15:48:51 +0530

    Monthly Slider added to app.py

--- server/app.py
+++ server/app.py
@@ -1,11 +1,48 @@
 from flask import Flask, request, jsonify
 import midas
-
+import requests
+import datetime
+import json
 app = Flask(__name__)
 
 
 def process_message(message):
     return {"message": message.upper()}
+
+
+def calculate_percentage_spent(transactions, budget):
+    total_spent = sum(transaction['amount'] for transaction in transactions if transaction['type'] == 'DEBIT')
+    percentage_spent = (total_spent / budget) * 100
+    return percentage_spent
+
+
+@app.route("/MonthlyBudgetslider", methods=["POST"])
+def monthly_budget_slider():
+    try:
+        # Get the monthly budget from the POST request data
+        request_data = request.json
+        budget = float(request_data.get('budget', 0))  # Replace 0 with a default value
+        
+        # Get the current month using the datetime module
+        current_month = datetime.datetime.now().month
+        
+        # Fetch transaction data from your local JSON file
+        with open('others/dataFetch.json', 'r') as data_file:
+            data = json.load(data_file)
+            transactions = data['Payload'][0]['data'][0]['decryptedFI']['account']['transactions']['transaction']
+            
+        # Filter transactions for the current month
+        current_month_transactions = [transaction for transaction in transactions if int(transaction['transactionTimestamp'][:10].split('-')[1]) == current_month]
+        
+        # Calculate the percentage spent
+        percentage_spent = calculate_percentage_spent(current_month_transactions, budget)
+        
+        # Post the calculated percentage back to the client as JSON response
+        response_data = {'percentage': percentage_spent}
+        return jsonify(response_data), 200
+        
+    except ValueError:
+        return "Invalid input. Please enter valid numerical values.", 400
 
 
 @app.route("/midasai", methods=["POST"])
